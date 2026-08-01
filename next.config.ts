855c48c0b52e881204cbc2105bf7d26138f39cc5
Ari-ICU <thoeurn.ratha.kh@gmail.com>
2026-08-01 15:38:11 +0700

    refactor: remove environment-specific production path configurations from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
@@ -9,9 +7,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-
-  basePath: isProd ? "/full-stack-short-coures" : "",
-  assetPrefix: isProd ? "/full-stack-short-coures/" : "",
 };
 
 export default nextConfig;
